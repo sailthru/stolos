ad2f5505cb1adcea459213387724b896c04d38bf
Alex Gaudio <adgaudio@gmail.com>
2014-06-20 23:30:19 -0400

    make scheduler execution more like spark

--- scheduler/bin/test_scheduler.sh
+++ scheduler/bin/test_scheduler.sh
@@ -1,6 +1,6 @@
-export TASKS_JSON="$HOME/ds/scheduler/examples/tasks.json"
-export JOB_ID_DEFAULT_TEMPLATE="{date}_{client_id}_{collection_name}"
-export JOB_ID_VALIDATIONS="tasks.job_id_validations"
+DIR="$( dirname "$( cd "$( dirname "$0" )" && pwd )")"
+
+. $DIR/conf/scheduler-env.sh
 
 nosetests \
 `python -c '
---
+++ scheduler/conf/scheduler-env.sh
@@ -0,0 +1,3 @@
+export TASKS_JSON="$DIR/examples/tasks.json"
+export JOB_ID_DEFAULT_TEMPLATE="{date}_{client_id}_{collection_name}"
+export JOB_ID_VALIDATIONS="tasks.job_id_validations"
