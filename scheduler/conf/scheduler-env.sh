d918c3da44096f182a44ec1c3905acb4f03189e4
Alex Gaudio <adgaudio@gmail.com>
2014-06-25 21:19:26 -0400

    fix a silly subtlety with bash setopt that breaks the conf

--- scheduler/conf/scheduler-env.sh
+++ scheduler/conf/scheduler-env.sh
@@ -1,4 +1,7 @@
-DIR="$( dirname "$( cd "$( dirname "$0" )" && pwd )")"
+#!/usr/bin/env bash
+
+DIR="$( dirname "$( cd "$( dirname "$0" )" &>/dev/null && pwd )")"
+
 export TASKS_JSON="$DIR/scheduler/examples/tasks.json"
 export JOB_ID_DEFAULT_TEMPLATE="{date}_{client_id}_{collection_name}"
 export JOB_ID_VALIDATIONS="scheduler.examples.job_id_validations"
