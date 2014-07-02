a61c4adc520aa606d5be06080c74867535bd68d5
Alex Gaudio <adgaudio@gmail.com>
2014-07-02 08:30:06 -0400

    crazy test stuff - trying to make tests randomizable and parallelizable with setup and teardown methods that modify the task names based on the function name.  in prog

--- scheduler/scheduler/tests/test_scheduler.py
+++ scheduler/scheduler/tests/test_scheduler.py
@@ -13,23 +13,78 @@ CMD = (
     'python -m scheduler.runner --zookeeper_hosts localhost:2181'
     ' -a {app_name} {extra_opts}'
 )
+TASKS_JSON = os.environ['TASKS_JSON']
+TASKS_JSON_TMPFILES = {}
 
 zk = zkt.get_client('localhost:2181')
 job_id1 = '20140606_1111_profile'
 job_id2 = '20140606_2222_profile'
-# TODO: remove this hardcoded value so we can parallelize tests
-app_name1 = 'test_scheduler/test_module'
-app_name2 = 'test_scheduler/test_module2'  # depends on app_name1 in tasks.json
+app_name1, app_name2, app_name3, app_name4 = [None] * 4
 
 
-def test_no_tasks():
+def create_tasks_json(fname_suffix='', inject={}, rename=False):
     """
-    The script shouldn't fail if it doesn't find any queued tasks
+    Create a new default tasks.json file
+    This of this like a useful setup() operation before tests.
+
+    `inject` - (dct) (re)define new tasks to add to the tasks graph
+    mark this copy as the new default.
+    `fname_suffix` - suffix in the file name of the new tasks.json file
+    `rename` - if True, change the name all tasks to include the fname_suffix
+
     """
+    tasks_dct = dt.get_tasks_dct()
+    tasks_dct.update(inject)
+
+    f = tempfile.mkstemp(prefix='tasks_json', suffix=fname_suffix)[1]
+    frv = ujson.dumps(tasks_dct)
+    if rename:
+        renames = [(k, "%s__%s" % (k, fname_suffix))
+                   for k in tasks_dct]
+        for k, new_k in renames:
+            frv = frv.replace(ujson.dumps(k), ujson.dumps(new_k))
+    with open(f, 'w') as fout:
+        fout.write(frv)
+
+    os.environ['TASKS_JSON'] = f
+    try:
+        dt.build_dag.cache.clear()
+    except AttributeError:
+        pass
+    return f
+
+
+def setup_func(func_name):
+    """Code that runs just before each test"""
     zk.delete('test_scheduler', recursive=True)
-    run_spark_code(app_name1)
-    validate_zero_queued_task(app_name1)
-    validate_zero_queued_task(app_name2)
+
+    # TODO: figure out how to make this ugly convenience hack
+    # better.  maybe functions should initialize app_name1 = get_app_name(name)
+    oapp_name1 = 'test_scheduler/test_module'
+    oapp_name2 ='test_scheduler/test_module2'
+    app_name1 = '%s__%s' % (oapp_name1, func_name)
+    app_name2 = '%s__%s' % (oapp_name2, func_name)
+    app_name3 = 'test_scheduler/test_depends_on__%s' % func_name
+    app_name4 = 'test_scheduler/test_bashworker__%s' % func_name
+    global app_name1, app_name2, app_name3, app_name4
+
+    f = create_tasks_json(fname_suffix=func_name, rename=True)
+    TASKS_JSON_TMPFILES[func_name] = f
+
+
+def teardown_func(func_name):
+    """Code that runs just after each test"""
+    os.environ['TASKS_JSON'] = TASKS_JSON
+    os.remove(TASKS_JSON_TMPFILES[func_name])
+
+
+def with_setup(func):
+    """A nose test-aware decorator that lets you specify
+    setup and teardown functions that know the name of the function they
+    perform setup and teardown for"""
+    setup = lambda: setup_func(func.func_name)
+    teardown = lambda: teardown_func(func.func_name)
+    return nose.tools.with_setup(setup, teardown)(func)
 
 
 @contextmanager
@@ -37,19 +92,7 @@ def _inject_into_dag(new_task_dct):
     """Update (add or replace) tasks in dag with new task config.
     This should reset any cacheing within the scheduler app,
     but it's not guaranteed"""
-    dg = dt.get_tasks_dct()
-    dg.update(new_task_dct)
-
-    f = tempfile.mkstemp(prefix='tasks_json')[1]
-    with open(f, 'w') as fout:
-        fout.write(ujson.dumps(dg))
-
-    old_f = os.environ['TASKS_JSON']
-    os.environ['TASKS_JSON'] = f
-    try:
-        dt.build_dag.cache.clear()
-    except AttributeError:
-        pass
+    f = create_tasks_json(inject=new_task_dct)
 
     # verify injection worked
     dg = dt.get_tasks_dct()
@@ -62,10 +105,20 @@ def _inject_into_dag(new_task_dct):
 
     yield
     os.remove(f)
-    dt.constants.TASKS_JSON = old_f
     dt.build_dag.cache.clear()
 
 
+@with_setup
+def test_no_tasks():
+    """
+    The script shouldn't fail if it doesn't find any queued tasks
+    """
+    run_spark_code(app_name1)
+    validate_zero_queued_task(app_name1)
+    validate_zero_queued_task(app_name2)
+
+
+@with_setup
 def test_create_child_task_after_one_parent_completed():
     # if you modify the tasks.json file in the middle of processing the dag
     # modifications to the json file should be recognized
@@ -73,7 +126,6 @@ def test_create_child_task_after_one_parent_completed():
     # the child task should run if another parent completes
     # but otherwise should not run until it's manually queued
 
-    zk.delete('test_scheduler', recursive=True)
     zkt.set_state(app_name1, job_id1, zk=zk, completed=True)
     validate_one_completed_task(app_name1, job_id1)
 
@@ -81,9 +133,7 @@ def test_create_child_task_after_one_parent_completed():
     dct = {
         injected_app: {
             "job_type": "bash",
-            "depends_on": {"app_name": [
-                "test_scheduler/test_module",
-                "test_scheduler/test_module2"]},
+            "depends_on": {"app_name": [app_name1, app_name2],},
         },
     }
     with _inject_into_dag(dct):
@@ -98,22 +148,23 @@ def test_create_child_task_after_one_parent_completed():
         validate_one_completed_task(injected_app, job_id1)
 
 
+@with_setup
 def test_create_parent_task_after_child_completed():
     # if you modify the tasks.json file in the middle of processing the dag
     # modifications to the json file should be recognized appropriately
 
     # we do not re-schedule the child unless parent is completed
 
-    zk.delete('test_scheduler', recursive=True)
     zkt.set_state(app_name1, job_id1, zk=zk, completed=True)
     validate_one_completed_task(app_name1, job_id1)
 
     injected_app = 'test_scheduler/test_module_ch'
+    child_injapp = 'test_scheduler/testX'
     dct = {
         injected_app: {
             "job_type": "bash",
         },
-        app_name1: {
+        child_injapp: {
             "job_type": "bash",
             "depends_on": {"app_name": [injected_app]}
         }
@@ -122,9 +173,10 @@ def test_create_parent_task_after_child_completed():
         validate_zero_queued_task(injected_app)
         zkt.set_state(injected_app, job_id1, zk=zk, completed=True)
         validate_one_completed_task(injected_app, job_id1)
-        validate_one_queued_task(app_name1, job_id1)
+        validate_one_queued_task(child_injapp, job_id1)
 
 
+@with_setup
 def test_should_not_add_queue_while_consuming_queue():
     """
     This test guards from doubly queuing jobs
@@ -138,12 +190,13 @@ def test_should_not_add_queue_while_consuming_queue():
     q.get()
     validate_one_queued_task(app_name1, job_id1)
 
-    enqueue(app_name1, job_id1, delete=False)
+    enqueue(app_name1, job_id1)
     with nose.tools.assert_raises(exceptions.JobAlreadyQueued):
         zkt.readd_subtask(app_name1, job_id1, zk=zk)
     validate_one_queued_task(app_name1, job_id1)
 
 
+@with_setup
 def test_push_tasks():
     """
     Child tasks should be generated and executed properly
@@ -158,6 +211,7 @@ def test_push_tasks():
     validate_one_queued_task(app_name2, job_id1)
 
 
+@with_setup
 def test_rerun_pull_tasks():
     # queue and complete app 1. it queues a child
     enqueue(app_name1, job_id1)
@@ -180,6 +234,7 @@ def test_rerun_pull_tasks():
     validate_one_completed_task(app_name2, job_id1)
 
 
+@with_setup
 def test_rerun_manual_task():
     enqueue(app_name1, job_id1)
     validate_one_queued_task(app_name1, job_id1)
@@ -193,6 +248,7 @@ def test_rerun_manual_task():
     validate_one_queued_task(app_name1, job_id1)
 
 
+@with_setup
 def test_rerun_push_tasks_when_manually_queuing_child_and_parent():
     _test_rerun_tasks_when_manually_queuing_child_and_parent()
 
@@ -209,6 +265,7 @@ def test_rerun_push_tasks_when_manually_queuing_child_and_parent():
     validate_one_completed_task(app_name2, job_id1)
 
 
+@with_setup
 def test_rerun_pull_tasks_when_manually_queuing_child_and_parent():
     _test_rerun_tasks_when_manually_queuing_child_and_parent()
 
@@ -254,6 +311,7 @@ def _test_rerun_tasks_when_manually_queuing_child_and_parent():
     validate_one_queued_task(app_name2, job_id1)
 
 
+@with_setup
 def test_rerun_push_tasks():
     # this tests recursively deleteing parent status on child nodes
 
@@ -289,41 +347,39 @@ def test_rerun_push_tasks():
     validate_one_completed_task(app_name2, job_id1)
 
 
+@with_setup
 def test_complex_dependencies_pull_push():
-    app_name = 'test_scheduler/test_depends_on'
     job_id = '20140601_1'
-    zk.delete('test_scheduler', recursive=True)
-    enqueue(app_name, job_id)
-    run_code(app_name, '--bash echo 123')
+    enqueue(app_name3, job_id)
+    run_code(app_name3, '--bash echo 123')
 
-    parents = list(sorted(dt.get_parents(app_name, job_id)))
+    parents = list(sorted(dt.get_parents(app_name3, job_id)))
     for parent, pjob_id in parents[:-1]:
         print(parent, pjob_id)
         consume_queue(parent)
         zkt.set_state(parent, pjob_id, zk=zk, completed=True)
-        validate_zero_queued_task(app_name)
+        validate_zero_queued_task(app_name3)
     print(parents[-1])
     consume_queue(parents[-1][0])
     zkt.set_state(*parents[-1], zk=zk, completed=True)
-    validate_one_queued_task(app_name, job_id)
-    run_code(app_name, '--bash echo 123')
-    validate_one_completed_task(app_name, job_id)
+    validate_one_queued_task(app_name3, job_id)
+    run_code(app_name3, '--bash echo 123')
+    validate_one_completed_task(app_name3, job_id)
 
 
+@with_setup
 def test_complex_dependencies_readd():
-    app_name = 'test_scheduler/test_depends_on'
     job_id = '20140601_1'
-    zk.delete('test_scheduler', recursive=True)
 
     # mark everything completed
-    parents = list(sorted(dt.get_parents(app_name, job_id)))
+    parents = list(sorted(dt.get_parents(app_name3, job_id)))
     for parent, pjob_id in parents:
         zkt.set_state(parent, pjob_id, zk=zk, completed=True)
     # --> parents should queue our app
-    validate_one_queued_task(app_name, job_id)
-    consume_queue(app_name)
-    zkt.set_state(app_name, job_id, zk=zk, completed=True)
-    validate_one_completed_task(app_name, job_id)
+    validate_one_queued_task(app_name3, job_id)
+    consume_queue(app_name3)
+    zkt.set_state(app_name3, job_id, zk=zk, completed=True)
+    validate_one_completed_task(app_name3, job_id)
 
     log.warn("OK... Now try complex dependency test with a readd")
     # re-complete the very first parent.
@@ -331,24 +387,25 @@ def test_complex_dependencies_readd():
     parent, pjob_id = parents[0]
     zkt.readd_subtask(parent, pjob_id, zk=zk)
     validate_one_queued_task(parent, pjob_id)
-    validate_zero_queued_task(app_name)
+    validate_zero_queued_task(app_name3)
     consume_queue(parent)
     zkt.set_state(parent, pjob_id, zk=zk, completed=True)
     validate_one_completed_task(parent, pjob_id)
-    # since that parent re-queues children that may be test_depends_on's
+    # since that parent re-queues children that may be app_name3's
     # parents, complete those too!
     for p2, pjob2 in dt.get_children(parent, pjob_id, False):
-        if p2 == app_name:
+        if p2 == app_name3:
             continue
         consume_queue(p2)
         zkt.set_state(p2, pjob2, zk=zk, completed=True)
     # now, that last parent should have queued our application
-    validate_one_queued_task(app_name, job_id)
-    run_code(app_name, '--bash echo 123')
-    validate_one_completed_task(app_name, job_id)
+    validate_one_queued_task(app_name3, job_id)
+    run_code(app_name3, '--bash echo 123')
+    validate_one_completed_task(app_name3, job_id)
     # phew!
 
 
+@with_setup
 def test_pull_tasks():
     """
     Parent tasks should be generated and executed before child tasks
@@ -377,6 +434,7 @@ def test_pull_tasks():
     validate_one_completed_task(app_name2, job_id1)
 
 
+@with_setup
 def test_retry_failed_task():
     """
     Retry failed tasks up to max num retries and then remove self from queue
@@ -385,7 +443,7 @@ def test_retry_failed_task():
     """
     # create 2 tasks in same queue
     enqueue(app_name1, job_id1)
-    enqueue(app_name1, job_id2, delete=False, validate_queued=False)
+    enqueue(app_name1, job_id2, validate_queued=False)
     nose.tools.assert_equal(2, get_zk_status(app_name1, job_id1)['app_qsize'])
     nose.tools.assert_equal(job_id1, cycle_queue(app_name1))
     # run job_id2 and have it fail
@@ -404,6 +462,7 @@ def test_retry_failed_task():
     validate_one_failed_task(app_name1, job_id2)
 
 
+@with_setup
 def test_invalid_task():
     """Invalid tasks should be automatically completed"""
     job_id = '20140606_3333_content'
@@ -411,26 +470,28 @@ def test_invalid_task():
     validate_one_completed_task(app_name2, job_id)
 
 
+@with_setup
 def test_valid_task():
     """Valid tasks should be automatically completed"""
     job_id = '20140606_3333_profile'
     enqueue(app_name2, job_id)
 
 
+@with_setup
 def test_bashworker():
     """a bash task should execute properly """
-    app_name = 'test_scheduler/test_bashworker'
     # queue task
-    enqueue(app_name, job_id1)
-    validate_one_queued_task(app_name, job_id1)
+    enqueue(app_name4, job_id1)
+    validate_one_queued_task(app_name4, job_id1)
     # run failing task
-    run_code(app_name, '--bash thiscommandshouldfail')
-    validate_one_queued_task(app_name, job_id1)
+    run_code(app_name4, '--bash thiscommandshouldfail')
+    validate_one_queued_task(app_name4, job_id1)
     # run successful task
-    run_code(app_name, '--bash echo 123')
+    run_code(app_name4, '--bash echo 123')
     validate_zero_queued_task(app_name)
 
 
+@with_setup
 def test_app_has_default_params():
     enqueue(app_name1, job_id1)
     msg = 'output: %s'
@@ -442,6 +503,7 @@ def test_app_has_default_params():
     nose.tools.assert_in("read_fp=''", logoutput, msg % logoutput)
 
 
+@with_setup
 def test_app_has_command_line_params():
     enqueue(app_name1, job_id1)
     msg = 'output: %s'
@@ -453,15 +515,13 @@ def test_app_has_command_line_params():
         'newfakereadfp', logoutput, msg % logoutput)
 
 
+@with_setup
 def test_failing_task():
     with nose.tools.assert_raises(Exception):
         run_spark_code(app_name1, '--jaikahhaha')
 
 
-def enqueue(app_name, job_id, delete=True, validate_queued=True):
-    if delete:
-        zk.delete('test_scheduler', recursive=True)
-
+def enqueue(app_name, job_id, validate_queued=True):
     # initialize job
     zkt.maybe_add_subtask(app_name, job_id, zk)
     zkt.maybe_add_subtask(app_name, job_id, zk)
