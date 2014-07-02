abf1a5820cc6f97aca09ef688676a994e839f752
Alex Gaudio <adgaudio@gmail.com>
2014-07-02 09:15:32 -0400

    DIR in the conf/ is confusing and doesn't work when you source files...

--- scheduler/conf/scheduler-env.sh
+++ scheduler/conf/scheduler-env.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "$0" )" &>/dev/null && pwd )"
-
+if [ -z "$DIR" ] ; then DIR='.' ; fi
 export TASKS_JSON="$DIR/scheduler/examples/tasks.json"
 export JOB_ID_DEFAULT_TEMPLATE="{date}_{client_id}_{collection_name}"
 export JOB_ID_VALIDATIONS="scheduler.examples.job_id_validations"
