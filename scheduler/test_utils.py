4100d1f4e9cd1256df8480c0ae2a770078d3376a
Alex Gaudio <adgaudio@gmail.com>
2014-08-30 01:01:49 -0400

    adds with_setup_args function to assist with tests

--- scheduler/test_utils.py
+++ scheduler/test_utils.py
@@ -26,3 +26,64 @@ def configure_logging(logname):
     log.handlers = [_h]  # log.addHandler(_h)
     log.setLevel(logging.DEBUG)
     return logging.getLogger(logname)
+
+
+def smart_run(func, args, kwargs):
+    """Given a function definition, determine which of the args and
+    kwargs are relevant and then execute the function"""
+    fc = func.func_code
+    args2 = args[:fc.co_argcount]
+    kwargs2 = {k: kwargs[k]
+                for k in fc.co_varnames[len(args2): len(args) + 1]}
+    print args2, kwargs2
+    func(*args2, **kwargs2)
+
+
+def with_setup_args(setup, teardown=None):
+    """Wraps nose.tools.with_setup, and support args/kwargs to test functions
+
+    Slightly modified from:
+    https://gist.github.com/garyvdm/392ae20c673c7ee58d76
+
+    Decorator to add setup and/or teardown methods to a test function::
+
+      @with_setup_args(setup, teardown)
+      def test_something():
+          " ... "
+
+      @with_setup_args(lambda: (['arg1', 'arg2'], {'kw1': 'a', 'kw2': 'b'}
+
+    The setup function should return (args, kwargs) which, if possible,
+    will be passed to test function, and teardown function.
+
+    Note that `with_setup_args` is useful *only* for test functions, not for
+    test methods or inside of TestCase subclasses.
+    """
+    def decorate(func):
+        args = []
+        kwargs = {}
+
+        def test_wrapped():
+            smart_run(func, args, kwargs)
+        test_wrapped.__name__ = func.__name__
+
+        def setup_wrapped():
+            a, k = setup()
+            args.extend(a)
+            kwargs.update(k)
+            if hasattr(func, 'setup'):
+                func.setup()
+        test_wrapped.setup = setup_wrapped
+
+        if teardown:
+            def teardown_wrapped():
+                if hasattr(func, 'teardown'):
+                    func.teardown()
+                smart_run(teardown, args, kwargs)
+
+            test_wrapped.teardown = teardown_wrapped
+        else:
+            if hasattr(func, 'teardown'):
+                test_wrapped.teardown = func.teardown()
+        return test_wrapped
+    return decorate
