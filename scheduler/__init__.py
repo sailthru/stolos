d4100e4e234e2e508c62e8f26eadf39ab6e7ac4a
Alex Gaudio <adgaudio@gmail.com>
2014-09-07 04:05:19 -0400

    add __version__ to scheduler

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -1,2 +1,7 @@
-import logging
-log = logging.getLogger('scheduler')
+import logging as _logging
+log = _logging.getLogger('scheduler')
+
+import os.path as _p
+import pkg_resources as _pkg_resources
+__version__ = _pkg_resources.get_distribution(
+    _p.basename(_p.dirname(_p.abspath(__file__)))).version
