1ab8cf0bcf647ef8f04008b34ec30bdbc03883bf
Alex Gaudio <adgaudio@gmail.com>
2014-12-18 16:09:31 -0500

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='stolos',
-    version='1.0.1',
+    version='1.0.2.dev0',
     description=(
         'A DAG-based job queueing system and executor for performing work'
         ' with complex dependency requirements between applications'),
