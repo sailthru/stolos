6733c8f5cc0203b7cd9402193b72c206f531f979
Alex Gaudio <adgaudio@gmail.com>
2015-11-18 18:39:27 -0500

    tests working in docker container

--- conf/stolos-env.sh
+++ conf/stolos-env.sh
@@ -13,13 +13,15 @@ export STOLOS_JOB_ID_VALIDATIONS="stolos.examples.job_id_validations"
 #
 
 export STOLOS_QUEUE_BACKEND="zookeeper"  # enabled by default
-export STOLOS_QB_ZOOKEEPER_HOSTS="localhost:2181"
+# export STOLOS_QB_ZOOKEEPER_HOSTS="localhost:2181"
+export STOLOS_QB_ZOOKEEPER_HOSTS="zk:2181"
 export STOLOS_QB_ZOOKEEPER_TIMEOUT=5
 
 # Redis queue backend
 # export STOLOS_QUEUE_BACKEND="majorityredis"
 export STOLOS_QB_REDIS_DB=0
-export STOLOS_QB_REDIS_HOSTS="localhost:6379"
+# export STOLOS_QB_REDIS_HOSTS="localhost:6379"
+export STOLOS_QB_REDIS_HOSTS="redis:6379"
 export STOLOS_QB_REDIS_N_SERVERS=1
 export STOLOS_QB_REDIS_SOCKET_TIMEOUT="5"
 export STOLOS_QB_REDIS_HISTORY_PREFIX="test_stolos"
@@ -40,7 +42,7 @@ export STOLOS_TASKS_JSON="$DIR/stolos/examples/tasks.json"
 # export STOLOS_CONFIGURATION_BACKEND="redis"
 # export STOLOS_REDIS_DB=0
 # export STOLOS_REDIS_PORT=6379
-# export STOLOS_REDIS_HOST='localhost'
+export STOLOS_REDIS_HOST='redis'
 
 # You can define your own custom configuration backend
 # export STOLOS_CONFIGURATION_BACKEND="mymodule.myconfiguration_backend"
