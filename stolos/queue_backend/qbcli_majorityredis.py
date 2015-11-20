9a203977a62546d06d8823fcb5ff47de28791859
Alex Gaudio <adgaudio@gmail.com>
2015-11-19 20:59:42 -0500

    re-implement the lock function using threading.Thread and majorityredis lua functions.  locking works, though may be a semi-bug where a lock isn't being explicitly released (probably by the runner)

--- stolos/queue_backend/qbcli_majorityredis.py
+++ stolos/queue_backend/qbcli_majorityredis.py
@@ -1,8 +1,14 @@
 from majorityredis import (MajorityRedis, retry_condition)
 from majorityredis.exceptions import Timeout
+import random
+import time
+import threading
+import os
+import signal
 import redis
 import sys
 
+from stolos import log
 from stolos import get_NS
 from stolos import argparse_shared as at
 from stolos import util
@@ -96,10 +102,118 @@ class LockingQueue(BaseLockingQueue):
         return self._q.is_queued(item=value)
 
 
+def _raise_err(x, y):
+    time.sleep(1)  # hack to better deal with cleanup
+    raise Exception("Received an exit signal from queue backend thread"
+                    " and now exiting")
+
+
 class Lock(BaseLock):
+    LOCKS = dict()  # path: client_id
+    SHAS = {}
+    SCRIPTS = dict(
+        # returns 1 if locked, 0 if could not lock.
+        # return exception if invalid expireat (ie lock is already expired)
+        l_lock=dict(keys=('path', ), args=('client_id', 'expireat'), script="""
+    if 1 == redis.call("SETNX", KEYS[1], ARGV[1]) then
+        if 1 == redis.call("EXPIREAT", KEYS[1], ARGV[2]) then return 1
+        else
+        redis.call("DEL", KEYS[1])
+        return {err="invalid expireat"} end
+    elseif ARGV[1] == redis.call("GET", KEYS[1]) then
+        if 1 ~= redis.call("EXPIREAT", KEYS[1], ARGV[2]) then
+        redis.call("DEL", KEYS[1])
+        return {err="invalid expireat"} end
+        return 1
+    else return 0 end
+    """),
+
+        # returns 1 if unlocked, 0 otherwise
+        l_unlock=dict(keys=('path', ), args=('client_id', ), script="""
+    local rv = redis.call("GET", KEYS[1])
+    if rv == ARGV[1] then
+        return redis.call("DEL", KEYS[1])
+    elseif rv == false then return 1
+    else return 0 end
+    """),
+
+        # returns 1 if got lock extended, 0 otherwise
+        l_extend_lock=dict(
+            keys=('path', ), args=('client_id', 'expireat'), script="""
+    if ARGV[1] == redis.call("GET", KEYS[1]) then
+        return redis.call("EXPIREAT", KEYS[1], ARGV[2])
+    else return {err="don't own lock"} end
+    """),
+    )
+    _INITIALIZED = False
+
     def __init__(self, path):
+        self._client_id = str(random.randint(0, sys.maxint))
         self._path = path
-        self._l = raw_client_mr().Lock()
+
+        self._lock_timeout = get_NS().qb_lock_timeout
+        self._max_network_delay = get_NS().qb_max_network_delay
+        if not Lock._INITIALIZED:
+            Lock._INITIALIZED = True
+            # don't replace any signal handlers if already set
+            for sig in [signal.SIGUSR1, signal.SIGUSR2,
+                        'fail-no signals availble']:
+                if signal.getsignal(sig) == 0:
+                    Lock._SIGNAL = sig
+                    break
+            signal.signal(Lock._SIGNAL, _raise_err)
+            # submit Lock lua scripts to redis
+            for k in Lock.SCRIPTS:
+                Lock.SHAS[k] = \
+                    raw_client().script_load(Lock.SCRIPTS[k]['script'])
+            # start extending locks in the background
+            t = threading.Thread(
+                name="queue_backend.majorityredis.Lock Extender",
+                target=self._extend_lock_in_background)
+            t.daemon = True
+            t.start()
+
+    def _extend_lock_in_background(self):
+        """
+        background signal (not a thread) that keeps lock alive
+        """
+        while True:
+            try:
+                s = time.time()
+                for path in list(Lock.LOCKS):
+                    try:
+                        client_id = Lock.LOCKS[path]
+                    except KeyError:
+                        continue  # race condition optimization
+                    extended = raw_client().evalsha(
+                        Lock.SHAS['l_extend_lock'],
+                        len(Lock.SCRIPTS['l_extend_lock']['keys']), path,
+                        client_id, int(time.time() + self._lock_timeout) + 1)
+                    if extended != 1 and path in Lock.LOCKS:
+                        raise Exception(
+                            "Failed to extend lock for path: %s. redis_msg: %s"
+                            % (path, extended))
+
+                # adjust sleep time based on min expireat
+                delta = time.time() - s
+                sleep_time = \
+                    self._lock_timeout - self._max_network_delay - delta
+                assert self._lock_timeout > sleep_time > 0, sleep_time
+                time.sleep(sleep_time)
+            except Exception as err:
+                # kill parent process
+                log.error(
+                    'Redis Queue Backend asking Stolos to exit(1)'
+                    ' because of err: %s' % err)
+                os.kill(os.getpid(), Lock._SIGNAL)
+                raise
+
+    def _acquire(self, path, client_id, nx=False, xx=False):
+        expireat = int(time.time() + self._lock_timeout)
+        return 1 == raw_client().evalsha(
+            Lock.SHAS['l_lock'],
+            len(Lock.SCRIPTS['l_lock']['keys']),
+            self._path, self._client_id, expireat)
 
     def acquire(self, blocking=False, timeout=None):
         """
@@ -110,9 +224,20 @@ class Lock(BaseLock):
             If True, wait up to `timeout` seconds to acquire a lock
         `timeout` (int) number of seconds.  By default, wait indefinitely
         """
-        if blocking and timeout is None:
-            timeout = sys.maxsize
-        return bool(self._l.lock(self._path, wait_for=timeout))
+        acquired = self._acquire(self._path, self._client_id, nx=True)
+
+        if not acquired and blocking:
+            c = 0
+            while True:
+                c += 1
+                time.sleep(1)
+                if c >= timeout:
+                    break
+                acquired = self._acquire(self._path, self._client_id, nx=True)
+
+        if acquired:
+            Lock.LOCKS[self._path] = self._client_id
+        return acquired
 
     def release(self):
         """
@@ -122,8 +247,22 @@ class Lock(BaseLock):
             - lock already released
             - lock does not exist (perhaps it was never acquired)
         """
-        if 50 > self._l.unlock(self._path):
-            raise UserWarning("Did not release lock")
+        if Lock.LOCKS.get(self._path) != self._client_id:
+            raise UserWarning("You must acquire lock before releasing it")
+        Lock.LOCKS.pop(self._path)
+
+        try:
+            rv = raw_client().evalsha(
+                Lock.SHAS['l_unlock'],
+                len(Lock.SCRIPTS['l_unlock']['keys']),
+                self._path, self._client_id)
+            assert rv == 1  # TODO
+        except AssertionError:
+            raise UserWarning("Lock did not exist on Redis server")
+        except Exception as err:
+            msg = "Could not release lock.  Got error: %s" % err
+            log.exception(msg, extra=dict(lock_path=self._path))
+            raise UserWarning(msg)
 
     def is_locked(self):
         """
@@ -203,6 +342,8 @@ build_arg_parser = at.build_arg_parser([
         help="Redis servers to connect to in form: host1:port1,host2:port2"
     ),
     at.add_argument('--qb_redis_db', default=0, type=int),
+    at.add_argument('--qb_lock_timeout', default=60, type=int),
+    at.add_argument('--qb_max_network_delay', default=30, type=int),
     at.add_argument(
         '--qb_redis_socket_timeout', default='5', type=float, help=(
             "number of seconds that the redis client will spend waiting for a"
