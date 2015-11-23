311406b7bf32d57ee1d2771acc064de0ca89ecb9
Alex Gaudio <adgaudio@gmail.com>
2015-11-23 11:39:08 -0500

    update conf for ..

--- conf/stolos-env.sh
+++ conf/stolos-env.sh
@@ -12,13 +12,8 @@ export STOLOS_JOB_ID_VALIDATIONS="stolos.examples.job_id_validations"
 # Queue Backend:
 #
 
-export STOLOS_QUEUE_BACKEND="zookeeper"  # enabled by default
-# export STOLOS_QB_ZOOKEEPER_HOSTS="localhost:2181"
-export STOLOS_QB_ZOOKEEPER_HOSTS="zk:2181"
-export STOLOS_QB_ZOOKEEPER_TIMEOUT=5
-
 # Redis queue backend
-# export STOLOS_QUEUE_BACKEND="majorityredis"
+export STOLOS_QUEUE_BACKEND="redis"  # enabled by default
 export STOLOS_QB_REDIS_DB=0
 # export STOLOS_QB_REDIS_HOSTS="localhost:6379"
 export STOLOS_QB_REDIS_HOSTS="redis:6379"
@@ -26,7 +21,14 @@ export STOLOS_QB_REDIS_N_SERVERS=1
 export STOLOS_QB_REDIS_SOCKET_TIMEOUT="5"
 export STOLOS_QB_REDIS_HISTORY_PREFIX="test_stolos"
 
-# You can define your own custom configuration backend
+# Zookeeper queue backend
+export STOLOS_QUEUE_BACKEND="zookeeper"
+# export STOLOS_QB_ZOOKEEPER_HOSTS="localhost:2181"
+export STOLOS_QB_ZOOKEEPER_HOSTS="zk:2181"
+export STOLOS_QB_ZOOKEEPER_TIMEOUT=5
+
+
+# You can define your own custom queue backend
 # export STOLOS_QUEUE_BACKEND="mymodule.myqueue_backend"
 
 
@@ -42,6 +44,7 @@ export STOLOS_TASKS_JSON="$DIR/stolos/examples/tasks.json"
 # export STOLOS_CONFIGURATION_BACKEND="redis"
 # export STOLOS_REDIS_DB=0
 # export STOLOS_REDIS_PORT=6379
+# export STOLOS_REDIS_HOST='localhost'
 export STOLOS_REDIS_HOST='redis'
 
 # You can define your own custom configuration backend
