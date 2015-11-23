c6c83f24e7da57baf8a168c44e05a5acb6d9c27c
Alex Gaudio <adgaudio@gmail.com>
2015-11-23 13:13:33 -0500

    adds docker script

--- bin/merge_branch.sh
+++
@@ -1,9 +0,0 @@
-set -e
-set -u
-
-branch=$1
-commit_msg=$2  # include things like "closes #issueNum"
-
-echo "running: git checkout master"
-git checkout master
-git merge --ff-only --no-ff -m "merge branch: $branch.  $commit_msg" $branch
